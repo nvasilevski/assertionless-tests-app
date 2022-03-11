e9f1291c35c415765bb2f472046b2b07ac6657f2
Nikita Vasilevsky <nikita.vasilevsky@shopify.com>
2022-03-11 22:09:22 +0000

    small refactoring

--- test/assertionless_tests_test.rb
+++ test/assertionless_tests_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class AssertionlessTestsTest < ActiveSupport::TestCase
   test "verifying a mock" do
-    mock = Minitest::Mock.new.expect(:my_method, 'return_value', ['arg_value'])
+    mock = Minitest::Mock.new
+    mock.expect(:my_method, 'return_value', ['arg_value'])
     mock.my_method('arg_value')
     mock.verify
   end
